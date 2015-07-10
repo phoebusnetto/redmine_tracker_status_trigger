c737b5feca53225ccdf3a77e02bcfa282383909d
jose.cavalcanti <jose.cavalcanti@phoebus.local>
2015-07-10 09:18:57 -0300

    Correção na criação do campo relation_tracker_id

--- db/migrate/001_create_tracker_status_trigger_setting.rb
+++ db/migrate/001_create_tracker_status_trigger_setting.rb
@@ -7,7 +7,7 @@ class CreateTrackerStatusTriggerSetting < ActiveRecord::Migration
       t.integer :upd_status_id_to, :null => false, :default => 0
       t.integer :status_id_change_from, :null => false, :default => 0
       t.integer :status_id_change_to, :null => false, :default => 0
-      t.integer :relation_tracker_id, :integer, :default => 0
+      t.integer :relation_tracker_id, :null => false, :default => 0
     end
   end
   
